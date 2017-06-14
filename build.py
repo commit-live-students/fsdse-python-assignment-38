341fe1188978cb99cbbb54d844f7ac77a74e68cb
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-06-14 21:42:02 +0000

    Done

--- build.py
+++ build.py
@@ -3,3 +3,16 @@ import itertools
 
 def solution(a_list):
     """Enter code here"""
+    sub = [[]]
+
+    for i in range(len(a_list)):
+        for j in range(len(a_list)):
+            if(i == j):
+                sub.append([a_list[i]])
+            elif(i > j):
+                sub.append([a_list[j],a_list[i]])
+    sub.append(a_list)
+    return sub
+
+
+solution([10, 20, 30])
