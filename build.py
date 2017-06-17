cd1b48f418d39da800267734248dfb63fd9e8c83
adinathauti <adinathauti@gmail.com>
2017-06-17 13:58:21 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,12 @@ import itertools
 
 
 def solution(a_list):
-    """Enter code here"""
+    comb = []
+    a_list.sort()
+    for i in range(len(a_list)+1):
+        for j in itertools.combinations(a_list,i):
+            comb.append(list(j))
+    print list(comb)
+    return comb
+
+solution([10,20,30])
