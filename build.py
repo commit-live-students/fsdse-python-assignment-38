dde3e6e8287fd1923180f682818354387cece9e4
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-17 01:19:11 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,21 @@ import itertools
 
 
 def solution(a_list):
-    """Enter code here"""
+    """Creation of sublist contains the check of possibility of new member
+        which is lying inside the orignal list,
+        then forming up the new list taking length of the as a constraint
+    """
+    sub_list = [[]]
+
+    for i in range(len(a_list)):
+        for j in range(len(a_list)):
+            if(i == j):
+                sub_list.append([a_list[i]])
+            elif(i > j):
+                sub_list.append([a_list[j],a_list[i]])
+    sub_list.append(a_list)
+    print sub_list
+    return sub_list
+
+
+solution([10, 20, 30])
