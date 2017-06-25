343a439f0dddfe4ff81dc71e173f5bcbe88f1d47
nikhilmborkar <nikhilmukundborkar@gmail.com>
2017-06-25 16:30:09 +0000

    Done

--- build.py
+++ build.py
@@ -3,3 +3,9 @@ import itertools
 
 def solution(a_list):
     """Enter code here"""
+    sublist_of_list = []
+    for i in range(0,len(a_list)+1):
+        sublist_of_list += map(list,itertools.combinations(a_list,i))
+
+    return sublist_of_list
+    
