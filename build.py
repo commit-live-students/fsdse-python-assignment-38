37c76176149c61d4dc0481c05a29b9894367796c
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-30 12:00:11 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,12 @@
 import itertools
 
+my_list = [10, 20, 30]
 
-def solution(a_list):
-    """Enter code here"""
+def solution(l):
+    mainList = []
+    for L in range(0,len(l)+1):
+        for subset in itertools.combinations(l, L):
+            mainList.append(list(subset))
+    return (mainList)
+
+solution(my_list)
