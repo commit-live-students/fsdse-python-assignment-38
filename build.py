00c85ab5288dd4c881fba9a0f43bd6b79273c86a
tejesh95 <tejeshpapineni95@gmail.com>
2017-07-03 04:44:25 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
-import itertools
-
+from itertools import chain, combinations
 
 def solution(a_list):
     """Enter code here"""
+    return [list(z) for z in chain.from_iterable(combinations(a_list, r) for r in range(len(a_list)+1))]
