f32a400632a9114bc4c03501bc300314e2b040ba
sagarambalam <sagar.ambalam@gmail.com>
2017-07-10 05:33:52 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,10 @@ import itertools
 
 
 def solution(a_list):
-    """Enter code here"""
+     array = []
+     a_list.sort()
+     for i in range(len(a_list)+1):
+        for j in itertools.combinations(a_list,i):
+            array.append(list(j))
+     #print list(comb)
+     return array
