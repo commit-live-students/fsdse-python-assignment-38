3e17e4dc0789653c6590c18e78efd4d2b1e67d76
sagard21 <sagard21@users.noreply.github.com>
2017-08-09 05:25:24 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,9 @@ import itertools
 
 
 def solution(a_list):
-    """Enter code here"""
+    l_subset = [[]]
+    for i in range(1, len(a_list)+1):
+        for subset in itertools.combinations(a_list, i):
+            lst = list(subset)
+            l_subset.append(lst)
+    return l_subset
