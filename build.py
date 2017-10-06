64fc2b98ad3c4e9247e57fe55c0523b73fa529bd
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-10-06 17:46:16 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 import itertools
-
-
 def solution(a_list):
     """Enter code here"""
+    combs = []
+    for i in range(0, len(a_list) + 1):
+        combs.extend([list(x) for x in itertools.combinations(a_list,i)])
+    return combs
